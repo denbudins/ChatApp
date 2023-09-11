fb46ed19467705ac8f9f53134f084fe6fbd26bab
Denis Martin Budinski <denis.budinski@intellegens.hr>
2023-09-11 09:03:17 +0200

    Version 2-4 finish

--- inputFunctions.ts
+++ inputFunctions.ts
@@ -1,41 +1,57 @@
 import { Room } from './models/room';
 import { Message } from './models/message';
+import { postToServer, outputMessages } from './outputFunctions';
 
-export function MyMessagesServices(inputMesseges: string[]): Room[] {
-  let allRooms: Room[] = [];
-  for (const messeg of inputMesseges) {
-    let newMesseg: string[] = paresMessage(messeg);
-    let room: Room | undefined = allRooms.find(({ id }) => id === newMesseg[2]);
-    if (room === undefined) {
-      room = new Room(newMesseg[2], []);
-      allRooms.push(room);
+export async function msgCallbackFn(state: Room[], msg: string) {
+  await wait(500 + 500 * Math.random()).then(() => {
+    MyMessagesServices(state, msg);
+  });
+}
+
+function MyMessagesServices(state: Room[], inputMesseges: string): void {
+  postToServer(inputMesseges);
+  let newMesseg: string[] = paresMessage(inputMesseges);
+  let room: Room | undefined = state.find(({ id }) => id === newMesseg[2]);
+  if (room === undefined) {
+    room = new Room(newMesseg[2], []);
+    state.push(room);
+    room.messages.push(new Message(undefined, `"${newMesseg[0]}" joined the room`, 'SERVER', new Date()));
+    outputMessages(`Announcement in room "${newMesseg[2]}"`, newMesseg[0], `"${newMesseg[0]}" joined the room`);
+    outputMessages(`"${newMesseg[0]}" posted in "${newMesseg[2]}"`, newMesseg[0], newMesseg[3]);
+  } else {
+    const uniqueUserNames: (string | undefined)[] = [
+      ...new Set(room.messages.filter(message => message.userName !== 'SERVER').map(message => message.userName)),
+    ];
+    if (room.messages.find(({ userName }) => userName === newMesseg[0]) === undefined) {
+      uniqueUserNames.push(newMesseg[0]);
       room.messages.push(new Message(undefined, `"${newMesseg[0]}" joined the room`, 'SERVER', new Date()));
-    } else {
-      if (room.messages.find(({ userName }) => userName === newMesseg[0]) === undefined) {
-        room.messages.push(new Message(undefined, `"${newMesseg[0]}" joined the room`, 'SERVER', new Date()));
+      for (const user of uniqueUserNames) {
+        outputMessages(`Announcement in room "${newMesseg[2]}"`, user!, `"${newMesseg[0]}" joined the room`);
       }
     }
-    room.messages.push(new Message(newMesseg[1], newMesseg[3], newMesseg[0], new Date()));
+    for (const user of uniqueUserNames) {
+      outputMessages(`"${newMesseg[0]}" posted in "${newMesseg[2]}"`, user!, newMesseg[3]);
+    }
   }
-  return allRooms;
+  room.messages.push(new Message(newMesseg[1], newMesseg[3], newMesseg[0], new Date()));
 }
 
 function paresMessage(inputMessege: string): string[] {
-  let paresedArray: string[] = splitOnRandomPieces(inputMessege, 2, ' ');
-  paresedArray = [...splitOnRandomPieces(paresedArray[0], 2, '@'), paresedArray[1]];
-  paresedArray = [...splitOnRandomPieces(paresedArray[0], 2, ':'), paresedArray[1], paresedArray[2]];
+  let paresedArray: string[] = splitOnRandomPieces(inputMessege, ' ', 2);
+  paresedArray = [...splitOnRandomPieces(paresedArray[0], '@', 2), paresedArray[1]];
+  paresedArray = [...splitOnRandomPieces(paresedArray[0], ':', 2), paresedArray[1], paresedArray[2]];
   return paresedArray;
 }
 
-function splitOnRandomPieces(stringToSplit: string, numberToSplit: number, characterForSplit: string): string[] {
-  let splittedArray: string[] = stringToSplit.split(characterForSplit);
-  if (splittedArray.length < numberToSplit) {
-    const rowsForAdd: number = numberToSplit - splittedArray.length;
+function splitOnRandomPieces(stringToSplit: string, separator: string, limit: number): string[] {
+  let splittedArray: string[] = stringToSplit.split(separator);
+  if (splittedArray.length < limit) {
+    const rowsForAdd: number = limit - splittedArray.length;
     for (let index: number = 0; index < rowsForAdd; index++) {
       splittedArray.push('');
     }
-  } else if (splittedArray.length > numberToSplit) {
-    const indexToPut: number = numberToSplit - 1;
+  } else if (splittedArray.length > limit) {
+    const indexToPut: number = limit - 1;
     let mergedString: string = '';
     for (let index: number = indexToPut; index < splittedArray.length; index++) {
       mergedString += `${splittedArray[index]} `;
@@ -44,4 +60,12 @@ function splitOnRandomPieces(stringToSplit: string, numberToSplit: number, chara
     splittedArray[indexToPut] = mergedString.trim();
   }
   return splittedArray;
+}
+
+async function wait(interval_in_ms: number): Promise<any> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(undefined);
+    }, interval_in_ms);
+  });
 }
--- outputFunctions.ts
+++ outputFunctions.ts
@@ -1,13 +1,15 @@
 import { Room } from './models/room';
 
-export function outputMessages(roomsArray: Room[]): string {
-  let outputText: string = '';
-  for (const room of roomsArray) {
-    outputText += `\nRoom  ${room.id}  messages: \n\n`;
-    for (const message of room.messages) {
-      outputText += `[${message.messageTime.toLocaleDateString()} ${message.messageTime.toLocaleTimeString()}] ${message.userName}: ${message.message} \n`;
-    }
-  }
+export function postToServer(msg: string): void {
+  console.log(`POSTing message: "${msg}"`);
+}
 
-  return outputText;
+export function outputMessages(typeOfMessage: string, userName: string, msg: string): void {
+  /*let outputText: string = '';
+  outputText += `\nRoom  ${room.id}  messages: \n\n`;
+  for (const message of room.messages) {
+    outputText += `[${message.messageTime.toLocaleDateString()} ${message.messageTime.toLocaleTimeString()}] ${message.userName}: ${message.message} \n`;
+  }*/
+
+  console.log(`To "${userName}" ::: ${typeOfMessage}: ${msg}`);
 }
